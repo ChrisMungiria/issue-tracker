2587992da6a115a3964bd14dcfc57902b39d5520
ChrisMungiria <christopher.mungiria@strathmore.edu>
2024-02-06 17:02:04 +0300

    feat: Register function

--- actions/register.ts
+++ actions/register.ts
@@ -1,10 +1,12 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
 
+import { db } from "@/db";
 import { RegisterSchema } from "@/schemas";
 
-export const register = (values: z.infer<typeof RegisterSchema>) => {
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
 
   if (!validatedFields.success) {
@@ -13,5 +15,28 @@ export const register = (values: z.infer<typeof RegisterSchema>) => {
     };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return {
+      error: "Email already in use",
+    };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return { success: "User created!" };
 };
