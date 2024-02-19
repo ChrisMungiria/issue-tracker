398a9b74a004bc57dea4da491170fb4aa0360154
ChrisMungiria <christopher.mungiria@strathmore.edu>
2024-02-19 15:53:25 +0300

    feat: added createdBy in the issues

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import prisma from "@/prisma/client";
+import { auth } from "@/auth";
 
 const createIssueSchema = z.object({
   title: z.string().min(1).max(255),
@@ -11,6 +12,9 @@ export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = createIssueSchema.safeParse(body);
 
+  // Get the uid
+  const uid = await auth().then((session) => session?.user.id);
+
   if (!validation.success) {
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
@@ -19,6 +23,11 @@ export async function POST(request: NextRequest) {
     data: {
       title: validation.data.title,
       description: validation.data.description,
+      createdBy: {
+        connect: {
+          id: uid,
+        },
+      },
     },
   });
 
