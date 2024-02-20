c27f65fa4847825d83d88d346c62ee96e2058441
ChrisMungiria <christopher.mungiria@strathmore.edu>
2024-02-20 14:40:53 +0300

    feat: fixed the loading animation

--- components/dashboard/customcard.tsx
+++ components/dashboard/customcard.tsx
@@ -40,8 +40,6 @@ const CustomCard = ({
         {loading ? (
           <div className="animate-pulse flex flex-col space-y-4">
             <div className="h-4 bg-gray-200 rounded w-1/2"></div>
-            <div className="h-4 bg-gray-200 rounded w-3/4"></div>
-            <div className="h-4 bg-gray-200 rounded w-1/2"></div>
           </div>
         ) : title === "Open issues" ? (
           <p>{issues.filter((issue) => issue.status === "OPEN").length}</p>
